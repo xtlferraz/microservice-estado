d83b1d831c96dc52cb990f6667a97e19011e224c
Thiago Lopes Ferraz <xtlferraz@gmail.com>
2020-03-31 15:19:43 -0300

    teste cors

--- index.js
+++ index.js
@@ -46,8 +46,8 @@ app.use((error, req, res, next) => {
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  //res.header("Access-Control-Allow-Origin", "http://localhost:4200/"); 
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Headers', 'content-type');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
