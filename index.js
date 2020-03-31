d24723548e33b3a1a2cdafe53c7658975b362cbe
Thiago <thiago.ferraz@dbccompany.com.br>
2020-03-31 15:54:40 -0300

    ajust cors

--- index.js
+++ index.js
@@ -47,11 +47,6 @@ app.use((error, req, res, next) => {
 
 
 app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 const porta = process.env.PORT || 3000;
 app.listen(porta);
