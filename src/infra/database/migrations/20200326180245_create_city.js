dbb84bfee178c457e690113e58fc038f7b04baa5
Thiago <thiago.ferraz@dbccompany.com.br>
2020-03-31 16:25:36 -0300

    ajust type database

--- src/infra/database/migrations/20200326180245_create_city.js
+++ src/infra/database/migrations/20200326180245_create_city.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
     return knex.schema.createTable('city',function(table){
         table.increments();
         table.string('name').notNullable();
-        table.string('population_quantity').notNullable();
+        table.integer('population_quantity').notNullable();
         table.integer('state_id').notNullable();
         table.foreign('state_id').references('id').inTable('state');
         table.unique(['state_id', 'name']);
